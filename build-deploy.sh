c805d3fcc0ce867230fe9b879a377f0ec9eca72e
AHP1609 <anuharish67@gmail.com>
2025-01-22 12:22:41 +0530

    build-deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo "enter the repo that is to be deployed"
-read repo
-rm -rf $repo
-echo "enter the url of the remote repo"
-read url
-git clone $url
-cd $repo
+
+rm -rf hello-world-war
+git clone https://github.com/AHP1609/hello-world-war.git  
+cd hello-world-war
 mvn clean package
+
+
 sudo cp /target/*.war /opt/tomcat/apache-tomcat-10.1.34/webapps/
 
 
